d66ee46200da7beca0bf3a128a719b165f27c84a
Auri <Atalonica@users.noreply.github.com>
2021-05-23 19:32:47 +0200

    Create board_config.h

---
+++ boards/tfg_zero/board_config.h
@@ -0,0 +1,14 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Atalonica"
+#define PRODUCT_NAME "TFG Zero"
+#define VOLUME_LABEL "TFGZEROBOOT"
+#define BOARD_ID "SAML21G18B-TFGZERO-v0"
+
+//#define USB_VID ?
+//#define USB_PID ?
+
+#define LED_PIN PIN_PA15
+
+#endif
